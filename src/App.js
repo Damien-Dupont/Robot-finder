bfefd403032b4f95a4d49d507ffe34c8604a4a34
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2022-05-05 11:34:24 +0200

    step2: CREATED cards, style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React from "react";
 /** Import de la donnée */
-import './App.css';
+import "./App.css";
 
-import Robots from './data/data';
+import Robots from "./data/data";
 
 function App() {
-  console.log(Robots)
+  console.log(Robots);
   return (
     <div className="App">
-      <h1>Robot Finder</h1>
+      <h1 style={{ color: "red", display: "flex" }}>Robot Finder</h1>
+      {Robots.map((droid) => (
+        <div
+          style={{
+            width: "60%",
+            justifyContent: "center",
+            backgroundColor: "cadetblue",
+            borderRadius: "20px",
+          }}
+        >
+          <h2 style={{ color: "whitesmoke" }}>
+            {droid.name} (aka {droid.username})
+          </h2>
+          <div style={{ backgroundColor: "lightcoral", borderRadius: "20px" }}>
+            <h3>Contact info:</h3>
+            <p>phone: {droid.phone}</p>
+            <p>eamil: {droid.email}</p>
+            <p>website: {droid.website}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
